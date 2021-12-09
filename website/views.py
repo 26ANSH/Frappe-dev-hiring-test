e3d812d3d9b533eb1cb8bba8c396f95f24fed14d
26ANSH <anshvidyabhanu8@gmail.com>
2021-12-10 03:06:44 +0530

    updated views

--- website/views.py
+++ website/views.py
@@ -4,7 +4,6 @@ from .models import Books
 from . import db
 import json
 
-URL = "https://frappe.io/api/method/frappe-library/?page="
 views = Blueprint('views', __name__)
 
 def page_not_found(e):
@@ -62,25 +61,3 @@ def logout():
     if "logged_in" in session:
         session.clear()
         return redirect(url_for('views.index', alert="Logged out Successfully."))
-
-@views.route('/book/<string:isbn>', methods=['GET'])
-def bookById(isbn):
-    print("got ISBN = ",isbn)
-    books = requests.get(URL+f"?isbn={isbn}").json()['message']
-    print(books)
-    if len(books) == 0:
-        return render_template('404.html')
-    return books[0]['title']
-
-@views.post('/get_books')
-def get_books():
-    params = json.loads(request.data)
-    print(type(params))
-    url = URL + str(params['page'])
-    for key in params.keys():
-        if key != 'page':
-            url += f"&{key}={params[key]}"
-
-    books = requests.get(url).json()['message']
-    print(books)
-    return jsonify(books)
